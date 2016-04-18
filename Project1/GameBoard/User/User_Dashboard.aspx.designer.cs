33380c3e3a229b623a527b02aad105b936a066b9
raul5660 <raul5660@gmail.com>
2016-04-18 15:32:00 -0500

    Added Dashboard Heading

--- Project1/GameBoard/User/User_Dashboard.aspx.designer.cs
+++ Project1/GameBoard/User/User_Dashboard.aspx.designer.cs
@@ -11,14 +11,5 @@ namespace Project1.GameBoard {
     
     
     public partial class WebForm2 {
-        
-        /// <summary>
-        /// ListView1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ListView ListView1;
     }
 }
